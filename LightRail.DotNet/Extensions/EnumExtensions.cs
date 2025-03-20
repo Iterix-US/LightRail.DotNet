cc36e3b033d7242026105763d4cb7bb2954bf608
Steven <142818284+sstarlin-gss@users.noreply.github.com>
2025-03-20 17:09:24 -0500

    Adding some more functions
    Security is coming from another library I have that's not netstandard 2.0, so there's some adaptation necessary.

---
+++ LightRail.DotNet/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace LightRail.DotNet.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<T>(this T enumValue) where T : Enum
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            return field?.GetCustomAttribute<DescriptionAttribute>()?.Description ?? enumValue.ToString();
+        }
+
+        public static List<T> GetAllValues<T>(this T enumValue) where T : Enum
+        {
+            return Enum.GetValues(enumValue.GetType()).Cast<T>().ToList();
+        }
+    }
+}
---
+++ LightRail.DotNet/SecurityUtilities/PasswordUtility.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Tasks;
+using LightRail.DotNet.Extensions;
+
+namespace LightRail.DotNet.SecurityUtilities
+{
+    public class PasswordUtility
+    {
+        private const int SaltSize = 16; // 128 bits
+        private const int KeySize = 32; // 256 bits
+        private const int Iterations = 8;
+        private const char SegmentDelimiter = ':';
+        private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA256;
+
+        public static byte[] BuildRandomSalt()
+        {
+            return GenerateRandomSalt();
+        }
+
+        private static byte[] GenerateRandomSalt()
+        {
+            var saltBytes = new byte[SaltSize];
+
+            using (var randomNumberGenerator = RandomNumberGenerator.Create())
+            {
+                randomNumberGenerator.GetBytes(saltBytes);
+            }
+
+            return saltBytes;
+        }
+
+        public static string SlowHash(string input, byte[] salt)
+        {
+            var hash = DeriveKey(input, salt);
+
+            return string.Join(
+                SegmentDelimiter,
+                hash.ToHexString(),
+                salt.ToHexString(),
+                Iterations,
+                Algorithm
+            );
+            return string.Empty;
+        }
+
+        public static byte[] DeriveKey(string password, byte[] salt, int iterations = 100_000, int keyLength = 32)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(password, salt, iterations))
+            {
+                return rfc2898.GetBytes(keyLength);
+            }
+        }
+
+        public static Task<bool> Verify(string input, string hashString)
+        {
+            return Task.Run(() =>
+            {
+                var segments = hashString.Split(SegmentDelimiter);
+                var hash = segments[0].ToHexString();
+                var salt = segments[1].ToHexString();
+                var iterations = int.Parse(segments[2]);
+                var algorithm = new HashAlgorithmName(segments[3]);
+
+                byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(
+                    input,
+                    salt,
+                    iterations,
+                    algorithm,
+                    hash.Length
+                );
+                return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+            });
+        }
+    }
+}
